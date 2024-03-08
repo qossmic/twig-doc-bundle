b4df27de37519d2ae684c2c1e199a36e2fa42e54
David Hölzel <56548897+davidhoelzel@users.noreply.github.com>
2024-03-08 12:48:51 +0100

    Create default variation based on parameters when none is configured
    It is now possible to have a component configuration without explicit variation definition. When no variations are given, a default one is created, based on the components parameters.

--- tests/Functional/Service/ComponentItemFactoryTest.php
+++ tests/Functional/Service/ComponentItemFactoryTest.php
@@ -2,14 +2,18 @@
 
 namespace Qossmic\TwigDocBundle\Tests\Functional\Service;
 
-use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\UsesClass;
+use Qossmic\TwigDocBundle\Component\ComponentItem;
 use Qossmic\TwigDocBundle\Component\ComponentItemFactory;
 use Qossmic\TwigDocBundle\Exception\InvalidComponentConfigurationException;
+use Qossmic\TwigDocBundle\Service\CategoryService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use TypeError;
 
-#[CoversNothing]
+#[CoversClass(ComponentItemFactory::class)]
+#[UsesClass(CategoryService::class)]
 class ComponentItemFactoryTest extends KernelTestCase
 {
     #[DataProvider('getInvalidComponentConfigurationTestCases')]
@@ -23,6 +27,83 @@ class ComponentItemFactoryTest extends KernelTestCase
         self::expectException($expectedExceptionClass);
 
         $service->create($componentData);
+    }
+
+    public function testComponentWithoutParameters(): void
+    {
+        $data = [
+            'name' => 'TestComponent',
+            'title' => 'Test title',
+            'description' => 'description',
+            'category' => 'MainCategory',
+        ];
+
+        /** @var ComponentItemFactory $factory */
+        $factory = self::getContainer()->get('twig_doc.service.component_factory');
+
+        $component = $factory->create($data);
+
+        self::assertInstanceOf(ComponentItem::class, $component);
+    }
+
+    public function testFactoryCreatesDefaultVariationWhenMissingInConfig(): void
+    {
+        $data = [
+            'name' => 'TestComponent',
+            'title' => 'Test title',
+            'description' => 'description',
+            'category' => 'MainCategory',
+        ];
+
+        /** @var ComponentItemFactory $factory */
+        $factory = self::getContainer()->get('twig_doc.service.component_factory');
+
+        $component = $factory->create($data);
+
+        self::assertArrayHasKey('default', $component->getVariations());
+    }
+
+    public function testFactoryCreatesDefaultVariationWithParameterTypes(): void
+    {
+        $data = [
+            'name' => 'TestComponent',
+            'title' => 'Test title',
+            'description' => 'description',
+            'category' => 'MainCategory',
+            'parameters' => [
+                'string' => 'String',
+                'float' => 'Float',
+                'double' => 'Double',
+                'int' => 'Int',
+                'integer' => 'Integer',
+                'bool' => 'Bool',
+                'boolean' => 'Boolean',
+                'unknown' => 'CustomType',
+                'complex' => [
+                    'title' => 'String',
+                    'amount' => 'Float',
+                ]
+            ]
+        ];
+
+        /** @var ComponentItemFactory $factory */
+        $factory = self::getContainer()->get('twig_doc.service.component_factory');
+
+        $component = $factory->create($data);
+
+        self::assertInstanceOf(ComponentItem::class, $component);
+        self::assertIsBool($component->getVariations()['default']['bool']);
+        self::assertIsBool($component->getVariations()['default']['boolean']);
+        self::assertIsString($component->getVariations()['default']['string']);
+        self::assertIsInt($component->getVariations()['default']['int']);
+        self::assertIsInt($component->getVariations()['default']['integer']);
+        self::assertIsFloat($component->getVariations()['default']['float']);
+        self::assertIsFloat($component->getVariations()['default']['double']);
+        self::assertNull($component->getVariations()['default']['unknown']);
+
+        self::assertIsArray($component->getVariations()['default']['complex']);
+        self::assertIsString($component->getVariations()['default']['complex']['title']);
+        self::assertIsFloat($component->getVariations()['default']['complex']['amount']);
     }
 
     public static function getInvalidComponentConfigurationTestCases(): iterable
