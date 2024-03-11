49d6bc4aa164a44e073b48e9950be5861304dcbc
david <david.hoelzel@gmx.de>
2024-03-11 12:46:33 +0100

    #3 run pipeline only on pull-request
    - use interface instead of class

--- src/Exception/InvalidComponentConfigurationException.php
+++ src/Exception/InvalidComponentConfigurationException.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 namespace Qossmic\TwigDocBundle\Exception;
 
-use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * @codeCoverageIgnore
  */
 class InvalidComponentConfigurationException extends \Exception
 {
-    public function __construct(private readonly ConstraintViolationList $violationList)
+    public function __construct(private readonly ConstraintViolationListInterface $violationList)
     {
         parent::__construct('TwigDocBundle: component configuration is invalid: '.$violationList);
     }
 
-    public function getViolationList(): ConstraintViolationList
+    public function getViolationList(): ConstraintViolationListInterface
     {
         return $this->violationList;
     }
